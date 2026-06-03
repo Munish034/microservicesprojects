c2a314d316376fb6039f60eea6cddb84af7d34ca
Munish034 <munishkumar034@gmail.com>
2026-06-03 15:15:58 +0530

    HLBS

--- UserServices/src/main/java/microservices/UserServices/controller/UserController.java
+++ UserServices/src/main/java/microservices/UserServices/controller/UserController.java
@@ -1,6 +1,7 @@
 package microservices.UserServices.controller;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import microservices.UserServices.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -31,17 +32,30 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED).body(user1);
 
     }
-    @GetMapping("/{userid}")
-    @CircuitBreaker(name = "userServiceBreaker",fallbackMethod = "userServiceBreakerFallback")
-    public ResponseEntity<User> getUser(@PathVariable  String userid){
+    @GetMapping("/test1")
+    @Retry(name = "ratingRetry")
+    public String test1() {
 
+        System.out.println(++count);
+
+        throw new RuntimeException("Test Retry");
+    }
+    int count=1;
+    @GetMapping("/{userid}")
+
+    @CircuitBreaker(name = "userServiceBreaker",fallbackMethod = "userServiceBreakerFallback")
+
+    public ResponseEntity<User> getUser(@PathVariable  String userid){
+        count++;
+        System.out.println(++count);
         User responseuser = userServices.getUser(userid);
+
 return ResponseEntity.ok(responseuser);
 
     }
     public  ResponseEntity<User> userServiceBreakerFallback(String userid,Exception ex){
 
-        User user = User.builder().name("manish").about("java developer").email("exceptions@").userid("12345").build();
+        User user = User.builder().name("manish").about("java developer").email("EXCEPTION.OCCURED@").userid("12345").build();
 return  new ResponseEntity<>(user,HttpStatus.OK);
 
 
